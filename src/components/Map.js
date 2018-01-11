9c2666a69f8af9f07746978cf268d56d04df3c54
Lee Mulvey <lmulvey@me.com>
2018-01-11 15:59:46 -0700

    Split into component.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,44 +1,5 @@
 import React, { Component } from 'react';
-
-class SummaryPanel extends Component {
-  render() {
-    const {
-      totalTrips, 
-      widgetLeftName,
-      widgetLeftData,
-      widgetMiddleName,
-      widgetMiddleData, 
-      widgetRightName,
-      widgetRightData,
-      panelDescription 
-    } = this.props.data;
-
-    return (
-      <div>
-      <p className="panelText">
-        {panelDescription}
-      </p>
-      <div className="left">
-        <p className="totalTrips">
-          {totalTrips}
-        </p>
-          <br/>Trips
-      </div>
-      <div className="right">
-        <div className={["widget", "red"].join(' ')}>
-          {widgetLeftName} ({widgetLeftData})
-        </div>
-        <div className={["widget", "yellow"].join(' ')}>
-          {widgetMiddleName} ({widgetMiddleData})
-        </div>
-        <div className={["widget", "green"].join(' ')}>
-        { widgetRightName}<br/>({widgetRightData})
-        </div>
-      </div>
-      </div>
-    );
-  }
-}
+import SummaryPanel from './SummaryPanel';
 
 class Map extends Component {
 
